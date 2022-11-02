afbaa5f35cf3023151fc9fa0191268754000ac16
Felix <barosiofelix@gmail.com>
2022-11-02 15:50:00 +0300

    Done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+headings = doc.css('.heading-financier').text.strip
+
+courses = doc.css('.heading-20-semibold.color-black.card-title-tl')
+
+courses.each do |course|
+  puts course.text.strip
+end
+
+
+
+# puts courses.text.strip
+
+# binding.pry
